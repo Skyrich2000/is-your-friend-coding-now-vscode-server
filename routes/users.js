595598b3f35fc26b62eef7373260399f8bdaff7b
huni <petercha2000@gmail.com>
2024-01-13 20:06:52 +0900

    .

--- routes/users.js
+++ routes/users.js
@@ -3,25 +3,43 @@ var router = express.Router();
 var fs = require("fs");
 
 router.get("/coding", function (req, res, next) {
-	const data = JSON.parse(fs.readFileSync("./coding.json", "utf8")) ?? [];
+  const data = JSON.parse(fs.readFileSync("./coding.json", "utf8")) ?? [];
 
-	res.send(data);
+  res.send(data);
 });
 
 router.post("/coding", function (req, res, next) {
-	const { user } = req.body;
+  const { user } = req.body;
+  if (!user) {
+    return res.send("");
+  }
 
-	if (!user) {
-		return res.send("");
-	}
+  const data = JSON.parse(fs.readFileSync("./coding.json", "utf8")) ?? [];
+  if (data.includes(user)) {
+    return res.send(data);
+  }
 
-	const data = JSON.parse(fs.readFileSync("./coding.json", "utf8")) ?? [];
+  data.push(user);
+  fs.writeFileSync("./coding.json", JSON.stringify(data));
 
-	data.push(user)
+  res.send(data);
+});
 
-	fs.writeFileSync("./coding.json", JSON.stringify(data))
+router.delete("/coding", function (req, res, next) {
+  const { user } = req.body;
+  if (!user) {
+    return res.send("");
+  }
 
-	res.send(data);
+  const data = JSON.parse(fs.readFileSync("./coding.json", "utf8")) ?? [];
+  if (!data.includes(user)) {
+    return res.send(data);
+  }
+
+  const newData = data.filter((u) => u !== user);
+  fs.writeFileSync("./coding.json", JSON.stringify(newData));
+
+  res.send(newData);
 });
 
 module.exports = router;
